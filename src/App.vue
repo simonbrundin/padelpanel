203c4307b677ea079d7aea765f2efe6190b23b79
Dennis Hamberg <simonbrundin@gmail.com>
2020-10-02 14:38:34 +0200

    starten till appen satt

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/totalpadel.png" class="logo" />
+
+    <div class="activities">
+      <div v-for="(aktivitet, index) in aktiviteter" class="aktivitetsrad">
+        {{ aktivitet.rubrik }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      aktiviteter: [
+        { rubrik: "Tävling", beskrivning: "Kalas tävling med magiska priser" },
+        { rubrik: "Öppet hus", beskrivning: "Kom och prova på padel" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
+body {
+  background: #000;
+}
+
 #app {
+  background: #000;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  position: absolute;
+  left: 0;
+  top: 0;
+  margin: 0 0 0 5vw;
+}
+
+.aktivitetsrad {
+  color: white;
 }
 </style>
